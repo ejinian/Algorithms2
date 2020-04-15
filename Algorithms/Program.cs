5c3d8e529061f6f4b68a7177cbe3acfc7a8e0545
sldghamr <thetankcoder@gmail.com>
2020-04-15 13:30:02 -0700

    initial commit

---
+++ Algorithms/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 1, 16, 4, 8 };
+            Searcher mySearcher = new Searcher();
+
+            //int myResult = mySearcher.Find(numbers, 10);
+            //int largestNumber = mySearcher.LargestNumberFinder(numbers);
+
+            //int secondLargestNumber = mySearcher.SecondLargestNumberFinder(numbers);
+
+            /*
+            int[] nums = new int[] { 2, 11, 14, 17 };
+            int target = 16;
+            
+            LeetCodeProgram1 myClass = new LeetCodeProgram1();
+            int [] result = myClass.TwoSum(nums, target);
+            result.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+            Console.ReadLine();
+            */
+
+            int x = 2147483647;
+
+            LeetCodeProgram2 myClass2 = new LeetCodeProgram2();
+            int reversed = myClass2.Reverse(x);
+
+            Console.WriteLine(reversed);
+            Console.ReadLine();
+        }
+    }
+}
+
+class LeetCodeProgram1
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { -1 };
+    }
+}
+
+class LeetCodeProgram2
+{
+    public int Reverse(int x)
+    {
+        int changeNumber;
+        int reversed = 0;
+        if (x > 2147483647)
+        {
+            return 0;
+        }
+        while (x != 0)
+        {
+            changeNumber = x % 10;
+            reversed = reversed * 10 + changeNumber;
+            x = x / 10;
+        }
+        if (reversed < 0)
+        {
+            return 0;
+        }
+        return reversed;
+    }
+}
---
+++ Algorithms/Searcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Searcher
+    {
+        public int Find(int[] numbers, int number) //time complexity is O(n)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int numberFromArray = numbers[i];
+                if (numberFromArray == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        
+        public int LargestNumberFinder(int[] numbers) //time complexity is O(n)
+        {
+            int largestNumberSoFar = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > largestNumberSoFar)
+                {
+                    largestNumberSoFar = numbers[i];
+                }
+            }
+            return largestNumberSoFar;
+        }
+
+        public int SecondLargestNumberFinder(int[] numbers)
+        {
+            int first, second;
+            first = second = int.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > first)
+                {
+                    second = first;
+                    first = numbers[i];
+                }
+                else if (numbers[i] > second && numbers[i] != first)
+                {
+                    second = numbers[i];
+                }
+            }
+            if (second == int.MinValue)
+            {
+                return -1;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
